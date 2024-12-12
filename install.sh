711891e54d7b40c9ffd9fb6457010b47c7109394
yuvraj1803 <ysakshith@gmail.com>
2024-12-12 19:17:18 +0530

    virt_tee: added libgnutls* dep resolve

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-sudo apt install python3-venv git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build
+sudo apt install libgnutls* python3-venv git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build
 git pull --recurse-submodules
